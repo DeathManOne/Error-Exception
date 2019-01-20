b056b295cab30a9953633af93995b43807973f07
DeathManOne <37964659+DeathManOne@users.noreply.github.com>
2019-01-20 17:53:37 +0100

    first version

--- error_exception.php
+++
@@ -1,3 +0,0 @@
-<?php
-set_error_handler(function ($code, $message, $file, $line)		{ new \DeathManOne\Flash($message, $code, $file, $line); });
-set_exception_handler(function ($code, $message, $file, $line)	{ new \DeathManOne\Flash($message, $code, $file, $line); });
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+set_error_handler(function ($code, $message, $file, $line)		{ new \DeathManOne\Flash($message, $code, $file, $line); });
+set_exception_handler(function ($code, $message, $file, $line)	{ new \DeathManOne\Flash($message, $code, $file, $line); });
+
 if ( \DeathManOne\Flash::$flash !== NULL )
 {
     foreach ( \DeathManOne\Flash::$flash as $error ):
